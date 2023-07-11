a7fb2963cedd135c86140c1f0e470a308cb4dca2
Juan Guerrero <juanvicente0104@gmail.com>
2023-07-11 20:54:12 +0000

    project delivery

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronouns = ["the", "our"];
+let adjectives = ["great", "big"];
+let nouns = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function domainGenerator(arrayOfWords1, arrayOfWords2, arrayOfWords3) {
+  let domains = [];
+  for (let wordInArray1 of arrayOfWords1) {
+    for (let wordInArray2 of arrayOfWords2) {
+      for (let wordInArray3 of arrayOfWords3) {
+        let webName = wordInArray1 + wordInArray2 + wordInArray3 + ".com";
+        console.log(webName);
+        domains.push(webName);
+      }
+    }
+  }
+  return domains;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//console.log("Now, we test our Domain Name Generator")
+domainGenerator(pronouns, adjectives, nouns);
